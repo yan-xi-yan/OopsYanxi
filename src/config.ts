63b0acf2b92f94804b79cf4c711699fd65ed80a6
yan-xi-yan <yanxi692@gmail.com>
2026-04-18 15:54:09 +0800

    修改网站标题描述

--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,7 @@ import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
 	title: "OopsYanxi",
-	subtitle: "Demo Site",
+	subtitle: "Blog Site",
 	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
